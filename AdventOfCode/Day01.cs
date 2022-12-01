2d8cbfff1a38e60b998c0eb0ab40c25aedd721a7
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-01 09:07:25 +0000

    solve first part (correct 70116)

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,5 +1,7 @@
 ﻿namespace AdventOfCode;
 
+using System.Diagnostics;
+
 public class Day01 : BaseDay
 {
     private readonly string[] _input;
@@ -11,7 +13,25 @@ public class Day01 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        return new("Not Solved");
+        int currentCal = 0;
+        int highestCal = 0;
+
+        foreach (string s in _input)
+        {
+            if (s.Length == 0)
+            {
+                currentCal = 0;
+            }
+            int lineCal;
+            Int32.TryParse(s, out lineCal);
+            currentCal += lineCal;
+            if (currentCal > highestCal)
+            {
+                highestCal = currentCal;
+            }
+        }
+
+        return new(highestCal.ToString());
     }
 
     public override ValueTask<string> Solve_2()
