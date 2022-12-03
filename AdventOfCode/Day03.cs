b6567a45f24559158ea4ad641e33da007303af7a
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-03 10:06:34 +0000

    solve first part (correct 8298)

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -1,4 +1,7 @@
-﻿namespace AdventOfCode;
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace AdventOfCode;
 
 public class Day03 : BaseDay
 {
@@ -11,12 +14,29 @@ public class Day03 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
+        int total = 0;
         foreach(string s in _input)
         {
+            int half = s.Length/2;
+            Debug.Assert((s.Length % 2) == 0);
+            byte[] first = Encoding.ASCII.GetBytes(s.Substring(0, half));
+            byte[] second = Encoding.ASCII.GetBytes(s.Substring(half , half));
+            Debug.Assert(first.Length == second.Length);
 
+            List<byte> common = first.Intersect(second).ToList();
+
+            int priority = 0;
+            foreach(byte b in common)
+            {
+                int p = b;
+                if (b < 91) p -= (64 - 26);
+                else p -= 96;
+                priority += p;
+            }
+            total += priority;
         }
 
-        return new("Not Solved");
+        return new(total.ToString());
     }
 
     public override ValueTask<string> Solve_2()
