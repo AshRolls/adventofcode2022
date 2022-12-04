f700ca3f1ec314b5641bdc6fc37f541dbcf4b3b3
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-04 08:35:27 +0000

    sovle first part (560)

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -6,12 +6,23 @@ public class Day04 : BaseDay
 
     public Day04()
     {
-        _input = File.ReadAllLines(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);        
     }
 
     public override ValueTask<string> Solve_1()
     {
-        return new("Not Solved");
+        int c = 0;
+        foreach (string s in _input)
+        {
+            string[] pair = s.Split(',');
+            int[] first = Array.ConvertAll(pair[0].Split('-'), str => int.Parse(str));
+            int[] second = Array.ConvertAll(pair[1].Split('-'), str => int.Parse(str));
+
+            if ((first[0] <= second[0]) && (first[1] >= second[1])) c++;
+            else if ((second[0] <= first[0]) && (second[1] >= first[1])) c++;
+        }
+
+        return new(c.ToString());
     }
 
     public override ValueTask<string> Solve_2()
