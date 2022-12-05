99ed63c0102f88bc65ebb425f1dbe5d8c4a0145b
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-05 09:52:04 +0000

    solve part two (NLCDCLVMQ)

--- AdventOfCode/Day05.cs
+++ AdventOfCode/Day05.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode;
 
@@ -51,6 +52,46 @@ public class Day05 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        return new("Not Solved");
+        Stack<char>[] st = new Stack<char>[9];
+        for (int i = 0; i < 9; i++) st[i] = new Stack<char>();
+
+        foreach (string s in _input)
+        {
+            if (s[1] == '1') break;
+            for (int i = 0; i < 9; i++)
+            {
+                int stackIdx = (i * 4) + 1;
+                if (s[stackIdx] != ' ') st[i].Push(s[stackIdx]);
+            }
+        }
+
+        for (int i = 0; i < 9; i++)
+        {
+            st[i] = new Stack<char>(st[i]);
+        }
+
+        Stack<char> q = new Stack<char>();
+        for (int i = 10; i < _input.Length; i++)
+        {
+            int[] move = Array.ConvertAll(Regex.Matches(_input[i], @"[0-9]+").OfType<Match>().Select(m => m.Groups[0].Value).ToArray(), x => int.Parse(x));
+            
+            for (int j = 0; j < move[0]; j++)
+            {
+                q.Push(st[move[1] - 1].Pop());
+            }
+            for (int j = 0; j < move[0]; j++)
+            {
+                st[move[2] - 1].Push(q.Pop());
+            }
+            Debug.Assert(q.Count() == 0);
+        }
+
+        string topCrates = String.Empty;
+        for (int i = 0; i < 9; i++)
+        {
+            topCrates += st[i].Pop();
+        }
+
+        return new(topCrates);
     }
 }
