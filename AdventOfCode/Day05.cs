ce4ed78868af57ed183c589e5618a7d6e317b6a2
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-05 08:58:00 +0000

    stub class and input

---
+++ AdventOfCode/Day05.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode;
+
+public class Day05 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day05()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        Stack<byte>[] st = new Stack<byte>[9];
+        
+        foreach (string s in _input)
+        {
+            if (s[1] == '1') break;
+            for (int i = 2; i< s.Length; i += 3)
+            {
+                
+            }
+        }
+
+        return new("Not Solved");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("Not Solved");
+    }
+}
