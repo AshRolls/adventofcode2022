60fa8399ac16a451c920cba2e12af9f3ea0d45c6
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-06 21:09:09 +0000

    add a dictionary to debug string helper

--- AdventOfCode/AoCHelper.cs
+++ AdventOfCode/AoCHelper.cs
@@ -4,7 +4,15 @@ namespace AdventOfCode
 {
     internal static class AoCHelper
     {
-        public static int[] NumsFromStr(string s) => Array.ConvertAll(Regex.Matches(s, @"[0-9]+").OfType<Match>().Select(m => m.Groups[0].Value).ToArray(), x => int.Parse(x));    
+        public static int[] NumsFromStr(string s) => Array.ConvertAll(Regex.Matches(s, @"[0-9]+").OfType<Match>().Select(m => m.Groups[0].Value).ToArray(), x => int.Parse(x));
+
         
+        public static string ToDebugString<TKey, TValue>(IDictionary<TKey, TValue> dictionary)
+        {
+            return "{" + string.Join(",", dictionary.Select(kv => kv.Key + "=" + kv.Value).ToArray()) + "}";
+            //Console.Out.WriteLine(ToDebugString<string,int>(clonedVisited) + " " + maxSmallCaves);
+        }
     }
+
+    
 }
