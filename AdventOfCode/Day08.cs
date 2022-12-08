9b8fc2132ec3c07ab8cf6e8f221da3a8f5d9d9cf
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-08 13:52:06 +0000

    solve part two (231112)
    messy vis needs cleaning

--- AdventOfCode/Day08.cs
+++ AdventOfCode/Day08.cs
@@ -1,11 +1,12 @@
 ﻿using AdventOfCode.vis;
+using Spectre.Console;
 
 namespace AdventOfCode;
 
 public class Day08 : BaseDay
 {
     private readonly string[] _input;
-    ASCIIRay renderer = new ASCIIRay(1920, 1080, 30, 10, "Day08");
+    ASCIIRay renderer = new ASCIIRay(1080, 1080, 30, 10, "Day08");
 
     public Day08()
     {
@@ -14,28 +15,23 @@ public class Day08 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        renderer.loop(x => render1(x));
+        //renderer.loop(x => solve1(x));
 
         return new("rendering");
     }
 
-    private bool render1(int idx)
+    public override ValueTask<string> Solve_2()
+    {
+        renderer.loop(x => solve2(x));
+
+        return new("Not Solved");
+    }
+
+    private bool solve1(int idx)
     {
         byte[,] grid = new byte[99, 99];
         bool[,] visible = new bool[99, 99];
-        string line;
-        for (int y = 0; y < 99; y++)
-        {
-            line = _input[y];
-            for (int x = 0; x < 99; x++)
-            {
-                bool vis = false;
-                if (x == 0 || x == 98 || y == 0 || y == 98) vis = true;
-                grid[x, y] = byte.Parse(line[x].ToString());
-                visible[x, y] = vis;
-                if (vis) renderer.WriteXY(x, y, "X");
-            }
-        }
+        parseData(grid, visible);
 
         for (int x = 1; x < 98; x++)
         {
@@ -45,6 +41,47 @@ public class Day08 : BaseDay
             }
         }
 
+        int visC = getVisibleCount(visible);
+
+        return false;
+    }
+
+    private bool solve2(int idx)
+    {
+        byte[,] grid = new byte[99, 99];
+        bool[,] visible = new bool[99, 99];
+        int[,] scenic = new int[99, 99];
+        parseData(grid, visible);
+
+        for (int x = 1; x < 98; x++)
+        {
+            for (int y = 1; y < 98; y++)
+            {
+                setScenic(grid, scenic, x, y);
+            }
+        }
+
+        int highS = getHighestScenic(scenic);
+
+        return false;
+    }
+
+    private int getHighestScenic(int[,] scenic)
+    {
+        int highest = 0;
+        for (int x = 1; x < 98; x++)
+        {
+            for (int y = 1; y < 98; y++)
+            {
+                if (scenic[x, y] > highest) highest = scenic[x,y];
+                renderer.WriteXY(x, y, scenic[x,y].ToString());
+            }
+        }
+        return highest;
+    }
+
+    private int getVisibleCount(bool[,] visible)
+    {
         int visC = 0;
         for (int y = 0; y < 99; y++)
         {
@@ -57,29 +94,46 @@ public class Day08 : BaseDay
                 }
             }
         }
-
-        return false;
+        return visC;
     }
 
-    public override ValueTask<string> Solve_2()
+    private void parseData(byte[,] grid, bool[,] visible)
     {
-        return new("Not Solved");
+        string line;
+        for (int y = 0; y < 99; y++)
+        {
+            line = _input[y];
+            for (int x = 0; x < 99; x++)
+            {
+                bool vis = false;
+                if (x == 0 || x == 98 || y == 0 || y == 98) vis = true;
+                grid[x, y] = byte.Parse(line[x].ToString());
+                visible[x, y] = vis;
+            }
+        }
     }
 
     private void setVisible(byte[,] grid, bool[,] visible, int x, int y)
     {
         // left
-        bool up = true, down = true, left = true, right = true;
-        for (int i = 0; i < x; i++)
+        bool left = true;
+        for (int i = x - 1; i >= 0; i--)
         {
-            if (grid[i,y] >= grid[x,y])
+            if (grid[i, y] >= grid[x, y])
             {
                 left = false;
                 break;
             }
-        }        
+        }
+        if (left) 
+        {
+            visible[x, y] = true; 
+            return;         
+        }
+
         // right
-        for (int i = 98; i > x; i--)
+        bool right = true;
+        for (int i = x + 1; i <= 98; i++)
         {
             if (grid[i, y] >= grid[x, y])
             {
@@ -87,8 +141,15 @@ public class Day08 : BaseDay
                 break;
             }                       
         }
+        if (right)
+        {
+            visible[x, y] = true;
+            return;
+        }
+
         // up
-        for (int i = 0; i < y; i++)
+        bool up = true;
+        for (int i = y - 1; i >= 0; i--)
         {
             if (grid[x, i] >= grid[x, y])
             {
@@ -96,8 +157,15 @@ public class Day08 : BaseDay
                 break;
             }            
         }
+        if (up)
+        {
+            visible[x, y] = true;
+            return;
+        }
+
         // down
-        for (int i = 98; i > y; i--)
+        bool down = true;
+        for (int i = y + 1; i <= 98; i++)
         {
             if (grid[x, i] >= grid[x, y])
             {
@@ -105,6 +173,61 @@ public class Day08 : BaseDay
                 break;
             }            
         }
-        visible[x, y] = up || down || left || right;
+        if (down)
+        {
+            visible[x, y] = true;
+            return;
+        }
     }
+
+    private void setScenic(byte[,] grid, int[,] scenic, int x, int y)
+    {
+        // left
+        int left = 0;
+        for (int i = x - 1; i >= 0; i--)
+        {
+            left++;
+            if (grid[i, y] >= grid[x, y])
+            {
+                break;
+            }            
+        }
+
+        // right
+        int right = 0;
+        for (int i = x + 1; i <= 98; i++)
+        {
+            right++;
+            if (grid[i, y] >= grid[x, y])
+            {
+                break;
+            }            
+        }
+
+        // up
+        int up = 0;
+        for (int i = y - 1; i >= 0; i--)
+        {
+            up++;
+            if (grid[x, i] >= grid[x, y])
+            {
+                break;
+            }
+        }
+
+
+        // down
+        int down = 0;
+        for (int i = y + 1; i <= 98; i++)
+        {
+            down++;
+            if (grid[x, i] >= grid[x, y])
+            {
+                break;
+            }
+        }
+
+        scenic[x, y] = left * right * up * down;            
+    }
+
 }
