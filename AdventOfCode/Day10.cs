633c204199c95e039246faed0391c9bde2035939
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-10 10:32:54 +0000

    solve part two (PGHFGLUG)
    (end of row is bugged)

--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -30,20 +30,14 @@ public class Day10 : BaseDay
         {
             if (i >= _input.Length) break;
             
-            if (_input[i][0] == 'n')
-            {
-                i++;
-            }
+            if (_input[i][0] == 'n') i++;
             else if (adding) 
             {
                 x += int.Parse(_input[i][5..]);
                 adding = false;
                 i++;
             }
-            else
-            {
-                adding = true;
-            }
+            else adding = true;            
 
             if (checkVals.Contains(c)) sum += c * x;
         }
@@ -59,6 +53,44 @@ public class Day10 : BaseDay
 
     private void solve2()
     {
+        int c = 1;
+        int x = 1;
+        int i = 0;
+        bool adding = false;
+        string screen = String.Empty;
+
+        while(true)
+        {
+            if (i >= _input.Length) break;
+
+            if (c%40 - x - 1 <= 1 && c%40 - x - 1>= -1)
+            {
+                screen += '\u2588';
+            }
+            else
+            {
+                screen += ' ';
+            }
+
+            if (_input[i][0] == 'n') i++;
+            else if (adding)
+            {
+                x += int.Parse(_input[i][5..]);
+                adding = false;
+                i++;
+            }
+            else adding = true;
+            c++;
+        }
+
+
+        Console.WriteLine(screen.Substring(0, 40));
+        Console.WriteLine(screen.Substring(40, 40));
+        Console.WriteLine(screen.Substring(80, 40));
+        Console.WriteLine(screen.Substring(120, 40));
+        Console.WriteLine(screen.Substring(160, 40));
+        Console.WriteLine(screen.Substring(200, 40));
+
         _partTwo = "Not Solved";
     }
 }
