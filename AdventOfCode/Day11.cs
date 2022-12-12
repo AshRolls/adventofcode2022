ec7ffa1df108e0ae02bde5ee45dcf0fdd92a78d8
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-12 12:19:55 +0000

    solve part two (17673687232)

--- AdventOfCode/Day11.cs
+++ AdventOfCode/Day11.cs
@@ -26,10 +26,7 @@ public class Day11 : BaseDay
     {
         Monkey[] monkeys = parseInput(); 
         
-        for (int i = 0; i < 20; i++)
-        {
-            performRound(monkeys);
-        }
+        for (int i = 0; i < 20; i++) performRound(monkeys, null);        
 
         var insp = monkeys.Select(x => x._inspected).ToList();
         insp.Sort();
@@ -48,12 +45,10 @@ public class Day11 : BaseDay
     {
         Monkey[] monkeys = parseInput();
 
-        Console.Out.WriteLine("starting");
-        for (int i = 0; i < 10000; i++)
-        {
-            Console.Out.Write(i.ToString() + " ");
-            performRound(monkeys, false);
-        }
+        // find product of all divisors to use a mod, reducing worry that doesn't effect throw logic
+        long val = monkeys.Select(x => x._testVal).Aggregate(1, (x, y) => x * y);
+
+        for (int i = 0; i < 10000; i++) performRound(monkeys, val);        
 
         var insp = monkeys.Select(x => x._inspected).ToList();
         insp.Sort();
@@ -62,34 +57,7 @@ public class Day11 : BaseDay
         _partTwo = (insp[0] * insp[1]).ToString();
     }
 
-    private Monkey[] parseInput()
-    {
-        Monkey[] monkeys = new Monkey[8];
-        for (int i = 0; i < 8; i++)
-        {
-            monkeys[i] = parseMonkey(_input[(i * 7)..(i * 7 + 6)]);
-        }
-        for (int i = 0; i < 8; i++)
-        {
-            setThrows(i, monkeys, _input[(i * 7)..(i * 7 + 6)]);
-        }
-        return monkeys;
-    }
-
-    static int sModM(string s, int mod)
-    {
-        int number = 0;
-        for (int i = 0; i < s.Length; i++)
-        {
-            number *= 10;
-            int x = int.Parse(s[i].ToString());
-            number += x;
-            number %= mod;
-        }
-        return number;
-    }
-
-    private void performRound(Monkey[] monkeys, bool worry = true)
+    private void performRound(Monkey[] monkeys, long? mod)
     {
         for (int i = 0; i < 8; i++)
         {
@@ -97,13 +65,22 @@ public class Day11 : BaseDay
             while (m._items.Any())
             {
                 m._inspected++;
-                BigInteger item = m._items.Dequeue();
+                long item = m._items.Dequeue();
                 item = m.Operation(item, m._opVal);
-                if (worry) item = item / 3; // Math.Floor automatically as all ints
+                if (mod == null) item /= 3; // Math.Floor automatically as all ints
+                else item %= (long)mod;
                 if (m.Test(item)) m._trueMonkey._items.Enqueue(item);
                 else m._falseMonkey._items.Enqueue(item);
             }
         }
+    }
+
+    private Monkey[] parseInput()
+    {
+        Monkey[] monkeys = new Monkey[8];
+        for (int i = 0; i < 8; i++) monkeys[i] = parseMonkey(_input[(i * 7)..(i * 7 + 6)]);        
+        for (int i = 0; i < 8; i++) setThrows(i, monkeys, _input[(i * 7)..(i * 7 + 6)]);
+        return monkeys;
     }
 
     private void setThrows(int i, Monkey[] monkeys, string[] strings)
@@ -127,32 +104,19 @@ public class Day11 : BaseDay
 
         Monkey m = new Monkey(opVal, opSelf, testVal);
 
-        foreach (int w in AoCHelper.NumsFromStr(strs[1]))
-        {
-            m._items.Enqueue(w);
-        }
+        foreach (int w in AoCHelper.NumsFromStr(strs[1])) m._items.Enqueue(w);
 
-        string op = strs[2].Split(' ')[^2];
-        if (op[0] == '+')
-        {
-            m.Operation = Monkey.OperationAdd;
-        }
-        else if (!opSelf)
-        {
-            m.Operation = Monkey.OperationMult;
-        }
-        else
-        {
-            m.Operation = Monkey.OperationMultSelf;
-        }
+        if (strs[2].Split(' ')[^2][0] == '+') m.Operation = Monkey.OperationAdd;        
+        else if (!opSelf) m.Operation = Monkey.OperationMult;
+        else m.Operation = Monkey.OperationMultSelf;
 
         return m;
     }
 
     private class Monkey
     {
-        public Queue<BigInteger> _items = new Queue<BigInteger>();
-        public Func<BigInteger, int, BigInteger> Operation;
+        public Queue<long> _items = new Queue<long>();
+        public Func<long, int, long> Operation;
         public bool _opSelf = false;
         public int _opVal;
         public int _testVal;
@@ -168,24 +132,24 @@ public class Day11 : BaseDay
             _inspected = 0;
         }
 
-        public bool Test(BigInteger val)
+        public bool Test(long val)
         {
             return (val % _testVal == 0);
         }
 
-        public static BigInteger OperationMult(BigInteger val, int op)
+        public static long OperationMult(long val, int op)
         {
-            return BigInteger.Multiply(val, op);
+            return val *= op;
         }
 
-        public static BigInteger OperationMultSelf(BigInteger val, int opUnused)
+        public static long OperationMultSelf(long val, int opUnused)
         {
-            return BigInteger.Pow(val, 2);
+            return val *= val;
         }
 
-        public static BigInteger OperationAdd(BigInteger val, int op)
+        public static long OperationAdd(long val, int op)
         {
-            return BigInteger.Add(val, op);
+            return val += op;
         }
     }
 }
