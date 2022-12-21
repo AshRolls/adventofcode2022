c3e5c6e8ad8879eeeecb5570e30f33e1e4e25f53
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-21 18:50:15 +0000

    solve part two (3759569926192) I'm not proud of it but it works

--- AdventOfCode/Day21.cs
+++ AdventOfCode/Day21.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode;
+﻿using System.ComponentModel.Design.Serialization;
+
+namespace AdventOfCode;
 
 public class Day21 : BaseDay
 {
@@ -13,32 +15,90 @@ public class Day21 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        solve1();
+        //solve1();
         return new(_partOne);
     }
 
     private void solve1()
     {
         Dictionary<string, long> broadcasting = new Dictionary<string, long>();
-        List<Monkey> waiting = new List<Monkey>();
+        HashSet<Monkey> waiting = new HashSet<Monkey>();
+        Monkey root = parseFile(broadcasting, waiting);
+        runMonkeys(broadcasting, waiting, root);
 
-        foreach (string line in _input)
+        _partOne = broadcasting["root"].ToString();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        solve2();
+        return new(_partTwo);
+    }
+
+    private void solve2()
+    {        
+        long p1 = 0;
+        long p2 = 1;
+        long lastp1;
+        long humnVal = 1;
+        GetVals(humnVal, out p1, out p2);
+        lastp1 = p1;
+
+        humnVal += 100000000;        
+        GetVals(humnVal, out p1, out p2);
+
+        long diff = lastp1 - p1;
+        humnVal = ((p1 - p2) / diff) * 100000000;
+        // above gets us reasonably close then hone in on the answer
+
+        long plusVal = 1000000;
+        long minusVal = 1000000;
+        bool plus = p1 > p2;
+        while (p1 != p2)
         {
-            var s = line.Split(':');
-            if (s[1].Length < 6) broadcasting.Add(s[0], long.Parse(s[1]));
+            GetVals(humnVal, out p1, out p2);
+
+            if (p1 == p2) break;
+            if (p1 > p2)
+            {
+                if (!plus && plusVal > 1)
+                    plusVal /= 10;
+                humnVal += plusVal;
+                plus = true;
+            }
             else
             {
-                var ms = s[1].Split(' ');
-                Monkey m = new Monkey(s[0], ms[1], ms[3], ms[2][0]);
-                waiting.Add(m);
+                if (plus && minusVal > 1)
+                    minusVal /= 10;
+                humnVal -= minusVal;
+                plus = false;
             }
         }
 
+        // note the val +1 above the actual answer will also mean p1 == p2. probably a bug in my solution with the divisor and rounding.
+        _partTwo = humnVal.ToString();
+    }
+
+    private void GetVals(long humnVal, out long p1, out long p2)
+    {
+        Dictionary<string, long> broadcasting = new Dictionary<string, long>();
+        HashSet<Monkey> waiting = new HashSet<Monkey>();
+        Monkey root = parseFile(broadcasting, waiting);
+        root.Op = '=';
+        broadcasting["humn"] = humnVal;
+        (long, long) result = runMonkeys(broadcasting, waiting, root, humnVal);
+        p1 = result.Item1;
+        p2 = result.Item2;
+    }
+
+    private static (long, long) runMonkeys(Dictionary<string, long> broadcasting, HashSet<Monkey> waiting, Monkey root, long humn = 1342)
+    {
         while (waiting.Count > 0)
         {
-            for (int i = 0; i < waiting.Count; i++)
+            List<Monkey> monkeys = waiting.ToList();
+            for (int i = 0; i < monkeys.Count; i++)
             {
-                Monkey m = waiting[i];
+                Monkey m = monkeys[i];
                 long v1;
                 long v2;
                 if (broadcasting.TryGetValue(m.M1, out v1) && broadcasting.TryGetValue(m.M2, out v2))
@@ -58,14 +118,34 @@ public class Day21 : BaseDay
                         case '/':
                             val = v1 / v2;
                             break;
+                        case '=':
+                            // do nothing
+                            break;
                     }
                     waiting.Remove(m);
                     broadcasting.Add(m.N, val);
                 }
             }
         }
+        return (broadcasting[root.M1], broadcasting[root.M2]);
+    }
 
-        _partOne = broadcasting["root"].ToString();
+    private Monkey parseFile(Dictionary<string, long> broadcasting, HashSet<Monkey> waiting)
+    {
+        Monkey root = new Monkey();
+        foreach (string line in _input)
+        {
+            var s = line.Split(':');
+            if (s[1].Length < 6) broadcasting.Add(s[0], long.Parse(s[1]));
+            else
+            {
+                var ms = s[1].Split(' ');
+                Monkey m = new Monkey(s[0], ms[1], ms[3], ms[2][0]);
+                waiting.Add(m);
+                if (m.N == "root") root = m;
+            }
+        }
+        return root;
     }
 
     private struct Monkey
@@ -84,14 +164,5 @@ public class Day21 : BaseDay
         }
     }
 
-    public override ValueTask<string> Solve_2()
-    {
-        solve2();
-        return new(_partTwo);
-    }
-
-    private void solve2()
-    {
-        _partTwo = "Not Solved";
-    }
+   
 }
--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 2022 fun and games, a playground for code hacked together with sticky tape and intuition.
 
 ## Visualisations
-Day 9 Part 2 on [YouTube](https://youtu.be/06Aru6dYwYI)
-Day 14 Part 2 on [YouTube](https://youtu.be/OTq5ORda5ho)
+Day 9 Part 2 on [YouTube](https://youtu.be/06Aru6dYwYI)  
+Day 14 Part 2 on [YouTube](https://youtu.be/OTq5ORda5ho)  
 
 ## The full disclosure I needed help section
-Day 11 Part 2, looked at other solutions as I couldn't work out the maths 'trick' to reduce all monkey values.
-Day 17 Part 2, looked at other solutions as I didn't think to cache the state looking for repeated patterns.
+Day 11 Part 2, looked at other solutions as I couldn't work out the maths 'trick' to reduce all monkey values.  
+Day 17 Part 2, looked at other solutions as I didn't think to cache the state looking for repeated patterns.  
