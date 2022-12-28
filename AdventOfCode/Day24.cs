864fdda02c2321b504903ac0bfa31ab44df46c58
Ashley Rolleston <AshRolls@users.noreply.github.com>
2022-12-28 15:09:29 +0000

    solve part two (877)

--- AdventOfCode/Day24.cs
+++ AdventOfCode/Day24.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.ComponentModel;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -17,17 +18,149 @@ public class Day24 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        solve1();
+        //solve1();
         return new(_partOne);
     }
 
     private void solve1()
     {
-        int w = _input[0].Length;
-        int h = _input.Length;
-        Cell[,] startGrid = new Cell[w, h];
+        int w, h;
+        Cell[,] startGrid;
+        Dictionary<char, (int, int)> dirs;
+        parseInput(out w, out h, out startGrid, out dirs);
 
-        Dictionary<char, (int, int)> dirs = new Dictionary<char, (int, int)>();
+        Dictionary<int, Cell[,]> minutes = new Dictionary<int, Cell[,]>();
+        minutes.Add(0, startGrid);
+        //printStorms(minutes[0], w, h);
+        (int x, int y) end = (w - 2, h - 1);
+
+        PriorityQueue<State, int> queue = new PriorityQueue<State, int>();
+        HashSet<State> visited = new HashSet<State>();
+        queue.Enqueue(new State(1, 0, 0, 0), AoCHelper.GetManhattanDist(1, 0, end.x, end.y));
+        int cnt = 0;
+        int best = int.MaxValue;
+        while (queue.Count > 0)
+        {
+            cnt++;
+            State state = queue.Dequeue();
+            if (state.X == end.x && state.Y == end.y)
+            {
+                if (state.M < best) best = state.M;
+            }
+            visited.Add(state);
+
+            // generate next round if we don't already have it
+            if (!minutes.ContainsKey(state.M + 1))
+            {
+                addMinute(state.M + 1, minutes, dirs, w, h);
+                //printStorms(minutes[m + 1], w, h);
+            }
+
+            // add valid moves to queue
+            int newX, newY, dist;
+            foreach ((int x, int y) dir in dirs.Values)
+            {
+                newX = state.X + dir.x;
+                newY = state.Y + dir.y;
+                dist = AoCHelper.GetManhattanDist(newX, newY, end.x, end.y);
+                if (((newX > 0 && newX < w && newY > 0 && newY < h) || (newX == 1 && newY == 0) || (newX == end.x && newY == end.y))
+                    && minutes[state.M + 1][newX, newY].Storms == String.Empty
+                    && !visited.Contains(new State(newX, newY, state.M + 1, 0))
+                    && (state.M + 1 + dist) < best)
+                {
+                    queue.Enqueue(new State(newX, newY, state.M + 1, 0), dist);
+                }
+            }
+            //if (cnt % 5000000 == 0) Console.Out.WriteLine("{0}: {1} {2}", cnt, state.M, AoCHelper.GetManhattanDist(state.X, state.Y, end.x, end.y));
+        }
+
+        _partOne = best.ToString();
+    }
+
+   
+
+    public override ValueTask<string> Solve_2()
+    {
+        solve2();
+        return new(_partTwo);
+    }
+
+    private void solve2()
+    {
+        int w, h;
+        Cell[,] startGrid;
+        Dictionary<char, (int, int)> dirs;
+        parseInput(out w, out h, out startGrid, out dirs);
+
+        Dictionary<int, Cell[,]> minutes = new Dictionary<int, Cell[,]>();
+        minutes.Add(0, startGrid);
+        //printStorms(minutes[0], w, h);
+        (int x, int y) start = (1, 0);
+        (int x, int y) end = (w - 2, h - 1);
+
+        PriorityQueue<State, int> queue = new PriorityQueue<State, int>();
+        HashSet<State> visited = new HashSet<State>();
+        int legDist = AoCHelper.GetManhattanDist(1, 0, end.x, end.y);
+        queue.Enqueue(new State(1, 0, 0, 2), legDist * 3);
+        int cnt = 0;
+        int best = int.MaxValue;
+        while (queue.Count > 0)
+        {
+            cnt++;
+            State state = queue.Dequeue();
+            visited.Add(state);
+
+            if (state.V == 0 && state.Y == end.y && state.X == end.x && state.M < best) best = state.M;            
+
+            // generate next round if we don't already have it
+            if (!minutes.ContainsKey(state.M + 1))
+            {
+                addMinute(state.M + 1, minutes, dirs, w, h);
+                //printStorms(minutes[m + 1], w, h);
+            }
+
+            
+            int newX, newY, newV, dist, gX, gY;
+            if (state.V == 1)
+            {
+                gX = start.x;
+                gY = start.y;
+            }
+            else
+            {
+                gX = end.x;
+                gY = end.y;
+            }
+            // add valid moves to queue
+            foreach ((int x, int y) dir in dirs.Values)
+            {
+                newX = state.X + dir.x;
+                newY = state.Y + dir.y;
+                newV = state.V;
+                if (state.V == 2 && newY == end.y && newX == end.x) newV--;
+                else if (state.V == 1 && newY == start.y && newX == start.x) newV--;
+                dist = AoCHelper.GetManhattanDist(newX, newY, gX, gY) + (newV * legDist);
+                if (((newX > 0 && newX < w && newY > 0 && newY < h) || (newX == 1 && newY == 0) || (newX == end.x && newY == end.y))
+                    && minutes[state.M + 1][newX, newY].Storms == String.Empty
+                    && !visited.Contains(new State(newX, newY, state.M + 1, newV))
+                    && (state.M + 1 + dist) < best)
+                {                                                        
+                    queue.Enqueue(new State(newX, newY, state.M + 1, newV), dist);
+                }
+            }
+            if (cnt % 100000 == 0) Console.Out.WriteLine("{0}: {1} {2}", cnt, state.M, state.V);
+        }
+
+        _partTwo = best.ToString();
+    }
+
+    private void parseInput(out int w, out int h, out Cell[,] startGrid, out Dictionary<char, (int, int)> dirs)
+    {
+        w = _input[0].Length;
+        h = _input.Length;
+        startGrid = new Cell[w, h];
+
+        dirs = new Dictionary<char, (int, int)>();
         dirs.Add('^', (0, -1));
         dirs.Add('v', (0, 1));
         dirs.Add('<', (-1, 0));
@@ -36,57 +169,23 @@ public class Day24 : BaseDay
 
         for (int y = 0; y < h; y++)
             for (int x = 0; x < w; x++)
-            {                
+            {
                 if (_input[y][x] != '.') startGrid[x, y] = new Cell(_input[y][x].ToString());
                 else startGrid[x, y] = new Cell(String.Empty);
             }
+    }
 
-        Dictionary<int, Cell[,]> minutes = new Dictionary<int, Cell[,]>();
-        minutes.Add(0, startGrid);
-        //printStorms(minutes[0], w, h);
+    private struct State
+    {
+        public int X, Y, M, V;
 
-        (int x, int y) end = (w - 2, h - 1);
-        PriorityQueue<(int,int,int), int> queue = new PriorityQueue<(int,int,int), int>();
-        HashSet<(int, int, int)> visited = new HashSet<(int, int, int)>();
-        queue.Enqueue((1,0,0), AoCHelper.GetManhattanDist(1, 0, end.x, end.y));        
-        int cnt = 0;
-        int best = int.MaxValue;
-        while (queue.Count > 0)
+        public State(int x, int y, int m, int v)
         {
-            cnt++;
-            (int x, int y, int m) state = queue.Dequeue();
-            if (state.x == end.x && state.y == end.y)
-            {
-                if (state.m < best) best = state.m;
-            }
-            visited.Add(state);
-
-            // generate next round if we don't already have it
-            if (!minutes.ContainsKey(state.m + 1))
-            {
-                addMinute(state.m + 1, minutes, dirs, w, h);
-                //printStorms(minutes[m + 1], w, h);
-            }
-
-            // add valid moves to queue
-            int newX, newY, dist;
-            foreach ((int x, int y) dir in dirs.Values)
-            {
-                newX = state.x + dir.x;
-                newY = state.y + dir.y;
-                dist = AoCHelper.GetManhattanDist(newX, newY, end.x, end.y);
-                if ( ((newX > 0 && newX < w && newY > 0 && newY < h) || (newX == 1 && newY == 0))
-                    && minutes[state.m + 1][newX, newY].Storms == String.Empty
-                    && !visited.Contains((newX, newY, state.m + 1)) 
-                    && (state.m + 1 + dist) < best)
-                {                    
-                    queue.Enqueue((newX, newY, state.m + 1), dist);
-                }
-            }
-            //if (cnt % 5000000 == 0) Console.Out.WriteLine("{0}: {1} {2}", cnt, state.m, AoCHelper.GetManhattanDist(state.x, state.y, end.x, end.y));
+            X = x;
+            Y = y;
+            M = m;
+            V = v;
         }
-
-        _partOne = best.ToString();
     }
 
     private void printStorms(Cell[,] minutes, int w, int h)
@@ -113,8 +212,7 @@ public class Day24 : BaseDay
         public Cell(string storms)
         {
             this.Storms = storms;
-        }
-        
+        }        
     }
 
     private void addMinute(int m, Dictionary<int, Cell[,]> minutes, Dictionary<char, (int, int)> stormDirs, int w, int h)
@@ -167,14 +265,5 @@ public class Day24 : BaseDay
         return (newX, newY);
     }  
 
-    public override ValueTask<string> Solve_2()
-    {
-        solve2();
-        return new(_partTwo);
-    }
 
-    private void solve2()
-    {
-        _partTwo = "Not Solved";
-    }
 }
